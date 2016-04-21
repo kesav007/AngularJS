266bbfd80a355581a26ca8be492d8ad7551ea3d9
Vaibhav <Vaibhav@kesav>
2016-04-21 16:30:44 -0400

    Initial Commit

--- angularjs-fundamentals-master/app/js/controllers/EventContoller.js
+++ angularjs-fundamentals-master/app/js/controllers/EventContoller.js
@@ -3,9 +3,9 @@ eventsApp.controller('EventController', function($scope) {
 	$scope.snippet ="<h2><font color='red'>hi there</font></h2>";
 	$scope.boolValue = false;
 	$scope.mystyle = {color:'red'};
-	$scope.disableBtn = true;
+	$scope.disableBtn = false;
     $scope.event = {
-            name: 'Angular Boot Camp',
+            name: 'Angular Boot Camp By Manju',
             date: '1/1/2013',
             time: '11:30 am',
             location: {
@@ -15,26 +15,26 @@ eventsApp.controller('EventController', function($scope) {
             },
             imageUrl: '/img/AngularJS_logo.png',
             sessions: [{
-                name: 'Directives Master Class',
+                name: 'Directives Master Class Kesav',
                 creatorName: 'Kesav',
-                duration: '1 hour',
+                duration: 1,
                 level: 'Advance',
                 abstract: 'In this session you will learn In and outs of directives.',
                 upVoteCount: 0
             }, {
                 name: 'Scopes for fun and profit',
                 creatorName: 'Nallan',
-                duration: '30 mins',
+                duration: 2,
                 level: 'Introductory',
                 abstract: 'This session will take you to closer look at scopes.',
                 upVoteCount: 0
             }, {
                 name: 'Well Behaved Controllers',
                 creatorName: 'Kesav Nallan',
-                duration: '2 hours',
+                duration: 4,
                 level: 'Intermediate',
                 abstract: 'Controllers are the beginng of everything.',
-                upVoteCount: 0
+                upVoteCount: 4
             }]
         };
 
--- angularjs-fundamentals-master/app/js/filters.js
+++ angularjs-fundamentals-master/app/js/filters.js
@@ -1,1 +1,15 @@
 'use strict';
+eventsApp.filter('duration_filter', function(){
+	return function(duration){
+		switch(duration){
+			case 1 :
+				return "1 Hour";
+			case 2 :
+				return "2 Hours";
+			case 3 :
+				return "Half Day";
+			default : 
+				return "Full Day";
+		}
+	}
+});
